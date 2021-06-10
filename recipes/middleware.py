fb58a078908045d854b57a8d7dee12cf82905987
Fedor Mityanin <79231292918@ya.ru>
2021-06-10 15:54:31 +0700

    after review

--- recipes/middleware.py
+++ recipes/middleware.py
@@ -8,7 +8,7 @@ def simple_middleware(get_response):
         request.tags = tag_list
         response = get_response(request)
         # с пагинацией придумал только так, как сделать функцией я не придумал
-        if request.GET.get('page'):
+        if request.GET.get('page') and response.status_code == 200:
             num_pages = response.context_data['paginator'].num_pages
             if int(request.GET.get('page')) > num_pages:
                 curent_url = request.path
